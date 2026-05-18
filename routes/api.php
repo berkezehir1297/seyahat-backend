9d0d1213b1fc1922e594fa6fa18d0aa9cf146aee
2400009169 Berke Zehir <2400009169@stu.iku.edu.tr>
2026-05-18 16:45:12 +0300

    fix: cache temizleme rotasi eklendi

--- routes/api.php
+++ routes/api.php
@@ -48,3 +48,8 @@ Route::get('/merhaba', function () {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/clear-cache', function() {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    return "Sunucu önbelleği başarıyla temizlendi!";
+});
